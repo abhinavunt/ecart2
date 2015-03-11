eeecdb1243955f7b4dd0fd8164d11a8b0e08fa68
Abhinav Shrivastava <abhinav.unt85@gmail.com>
2015-03-11 19:44:14 +0530

    Update ItemCtrl.js

--- ecart/public/js/controllers/ItemCtrl.js
+++ ecart/public/js/controllers/ItemCtrl.js
@@ -1,7 +1,9 @@
 
-angular.module('ItemCtrl',[]).controller('ItemController', function($scope,$http,ngDialog,$upload) {
+angular.module('ItemCtrl',['ngImgCrop']).controller('ItemController', function($scope,$http,ngDialog,$upload) {
 	
 	$scope.submitButtonVal=false;
+	$scope.submitButtonValEdit=false;
+	$scope.removeButtonValEdit=false;
 	$scope.addItemButtonVal=true;
 	$scope.searchItemVal=true;
 	$scope.amountPriceRow=[];
@@ -74,11 +76,10 @@ angular.module('ItemCtrl',[]).controller('ItemController', function($scope,$http
 	    
 	    $scope.searchItems = function(name){
 	    	
-	    	$scope.menuLevelTwoName = name;
-	        
-	    	$scope.category = 'Category:- '+$scope.menuLevelZeroName+' > '+$scope.menuLevelOneName+' > '+ $scope.menuLevelTwoName;
-	    	$scope.category2 = $scope.menuLevelZeroName+' > '+$scope.menuLevelOneName+' > '+ $scope.menuLevelTwoName;
 	    	
+	    	$scope.menuLevelTwoName = name;
+	        $scope.category = 'Category:- '+$scope.menuLevelZeroName+' > '+$scope.menuLevelOneName+' > '+ $scope.menuLevelTwoName;
+	    	$scope.category2 = $scope.menuLevelZeroName+' > '+$scope.menuLevelOneName+' > '+ $scope.menuLevelTwoName;
 	    	$scope.addItemButtonVal=false;
 	    	$scope.searchItemVal=false;
 	    	
@@ -134,7 +135,7 @@ angular.module('ItemCtrl',[]).controller('ItemController', function($scope,$http
 	  	    			brand:$scope.itemForm.brand,
 	  	    			othernames:$scope.itemForm.otherNames,
 	  	    			description:$scope.itemForm.description,
-	  	    			availablity:$scope.itemForm.availabilityCheck,
+	  	    			availability:$scope.itemForm.availabilityCheck,
 	  	    			amountprice:$('#amtPriceTableId').tableToJSON(),
 	  	    			imageId:ImgId
 	  	    		};
@@ -145,10 +146,13 @@ angular.module('ItemCtrl',[]).controller('ItemController', function($scope,$http
 	  	            data: JSON.stringify(item),
 	  	            headers: {'Content-Type': 'application/json'}
 	  	          }).success(function (data, status, headers, config,imageName) {
-	  	        	  
+	  	        	
+	  	        	//$scope.searchItems($scope.menuLevelTwoName);
+	  	        	
+	  	        	$scope.itemList.unshift(item);
 	  	        	$scope.submitButtonVal=true;
 	  	        	$scope.closeThisDialog();
-	  	        	searchItems($scope.menuLevelTwoName);
+	  	        	
 	  	        	
 	  	          }).error(function (data, status, headers, config) {
 	  	              
@@ -156,4 +160,84 @@ angular.module('ItemCtrl',[]).controller('ItemController', function($scope,$http
 	           
 	          });
 	    	};
-	});
+	    	
+	    $scope.editItem = function(item){
+	    	
+	    	if(typeof $scope.newImg=='undefined'){
+	    		
+	    	 var item ={
+		  	    			itemId:item._id,
+		  	    			category:$scope.menuLevelTwoName,
+		  	    			name:$scope.itemDetails.name,
+		  	    			brand:$scope.itemDetails.brand,
+		  	    			othernames:$scope.itemDetails.othernames,
+		  	    			description:$scope.itemDetails.description,
+		  	    			availability:$scope.itemDetails.availabilityCheck,
+		  	    			amountprice:$('#amtPriceTableId').tableToJSON(),
+		  	    			imageId:item.imageId
+		  	    		};
+	    	 
+	    	 
+	    		 
+	    		 $http({
+		  	            url: '/item/editItem',
+		  	            method: "POST",
+		  	            data: JSON.stringify(item),
+		  	            headers: {'Content-Type': 'application/json'}
+		  	          }).success(function (data, status, headers, config,imageName) {
+		  	        	$scope.submitButtonValEdit=true;
+		  	        	$scope.closeThisDialog();
+		  	        	$scope.searchItems($scope.menuLevelTwoName);
+		  	          }).error(function (data, status, headers, config) {
+		  	              
+		  	          });
+	    		
+	    	}else{
+	    		
+	    	}
+	    };
+	    
+	    $scope.removeItem = function(item){
+	    	$http({
+		  	            url: '/item/removeItem',
+		  	            method: "POST",
+		  	            data: {itemId:item._id},
+		  	            headers: {'Content-Type': 'application/json'}
+		  	          }).success(function (data, status, headers, config,imageName) {
+			  	        	$scope.removeButtonValEdit=true;
+			  	        	$scope.closeThisDialog();
+			  	        	var index=$scope.itemList.indexOf(item);
+			  	        	$scope.itemList.splice(index,1);
+		  	          }).error(function (data, status, headers, config) {
+		  	        	alert("Failed : Item could not be removed !!!");
+		  	          });
+	    };
+	    
+	    
+	    
+	    
+	    		
+	    $scope.editItemOpenPopUp = function(item){
+	    		$scope.itemDetails = item;
+	    		$scope.amountPriceRowEdit = item.amountprice;
+	    		$scope.itemImageEdit = 'temp_upload/'+item.imageId;
+	    		
+	    		var dialog = ngDialog.open({
+	    	            template: 'views/adminTemplates/editItemPopUp.html',
+	    	            scope: $scope,
+	    	            className: 'ngdialog-theme-default'
+	    	    });
+	    }
+	    
+	    $scope.addAmountPriceRowEdit = function() {
+			 var newRow = { "Amount" : "","Price" : "", "Availability" : "Available"};
+			 $scope.amountPriceRowEdit.push(newRow);
+		};
+	    
+	    $scope.deleteAmountPriceRowEdit = function(index) {
+			
+			$scope.amountPriceRowEdit.splice(index,1);
+		};
+	
+
+});
