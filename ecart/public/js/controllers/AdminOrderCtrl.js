2623fa38118ea1d15b5c7a24dc2b3b75a2a712ed
abhinav shrivastava <abhinav.unt85@gmail.com>
2015-04-13 18:46:57 +0530

    pagination started

--- ecart/public/js/controllers/AdminOrderCtrl.js
+++ ecart/public/js/controllers/AdminOrderCtrl.js
@@ -1,19 +1,22 @@
 // public/js/controllers/MainCtrl.js
 angular.module('AdminOrderCtrl', []).controller('AdminOrderController', function($scope,$http,ngDialog) {
 	
+	$scope.orderPerPageList = [{'order':10},{'order':25},{'order':50}];
+	$scope.orderPerPage = $scope.orderPerPageList[0].order;
 	
-	$http.get('/order/orderList')
-	.success(function(data) {
-		
-		$.each(data, function(){
-			
-			$scope.orderlist = data;
-			console.log(data);
-		  });
-		})
-		.error(function(data) {
+	
+	$http({
+            url: '/order/orderList',
+            method: "GET",
+            params: {year: yearVal}
+         }).success(function(data) {
+        	 $scope.orderlist = data;
+        	
+		 }).error(function(data) {
 			console.log('Error: ' + data);
-		});
+		 });
+		
+	//http://blog.mongodirector.com/fast-paging-with-mongodb/
 	
 	$scope.myFunction = function(order){
 		$scope.showOrderList = order;
@@ -23,5 +26,9 @@ angular.module('AdminOrderCtrl', []).controller('AdminOrderController', function
             scope: $scope,
             className: 'ngdialog-theme-default'
           });
+	}
+	
+	$scope.selectedOrderPerPage = function(orderPerPageObj){
+		$scope.orderPerPage = orderPerPageObj.order;
 	}
 });
