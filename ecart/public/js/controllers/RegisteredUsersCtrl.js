5f3326d38614bcefadcca71cd41284979183caca
Sony <Sony@192.168.2.4>
2015-07-07 01:16:21 +0530

    impro migrated to angular1.4

--- ecart/public/js/controllers/RegisteredUsersCtrl.js
+++ ecart/public/js/controllers/RegisteredUsersCtrl.js
@@ -1,9 +1,10 @@
 angular.module('RegisteredUsersCtrl', []).controller('RegisteredUsersController', function($scope,$http) {
-
-	    $http.get('/user/getUsers')
-		.success(function(data) {
-			$scope.userlist = data;
-		}).error(function(data) {
-			console.log('Error: ' + data);
-		});
+	$scope.showItemList=[{"itemPerPage":"10 Users/Page","value":10},{"itemPerPage":"20 Users/Page","value":20},{"itemPerPage":"30 Users/Page","value":30}]
+	
+    $http.get('/user/getUsers')
+	.success(function(data) {
+		$scope.userlist = data;
+	}).error(function(data) {
+		console.log('Error: ' + data);
+	});
 });
