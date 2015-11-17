69e1377e89c04efebecd821aaf7e05b3a4f0e527
Sony <Sony@Sony-VAIO>
2015-11-18 01:38:00 +0530

    imp

--- ecart/server.js
+++ ecart/server.js
@@ -6,7 +6,7 @@ var methodOverride = require('method-override');
 var nodemailer = require('nodemailer');
 //var cloudinary = require('cloudinary');
 var MSON = require('mongoson');
-var compression = require('compression'); 
+//var compression = require('compression'); 
 
 var path = require('path');
 var fs = require('fs');
@@ -42,7 +42,7 @@ app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 app.use(multer({ dest: __dirname +'/public/temp_upload/'}));
-app.use(compression());
+//app.use(compression());
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
 
